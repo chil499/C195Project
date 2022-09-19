9c919d1dd7549d31f9271bbf70d8a9d80c94dbcf
LabUser <chil499@wgu.edu>
2022-09-19 13:30:29 -0600

    fifth save

---
+++ src/helper/JDBC.java
@@ -0,0 +1,17 @@
+package helper;
+
+import java.sql.Connection;
+
+public abstract class JDBC {
+
+    private static final String protocol = "jdbc";
+    private static final String vendor = ":mysql:";
+    private static final String location = "//localhost/";
+    private static final String databaseName = "client_schedule";
+    private static final String jdbcUrl = protocol + vendor + location + databaseName + "?connectionTimeZone = SERVER";
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+    private static final String userName = "sqlUser";
+    private static final String password = "Passw0rd!";
+    public static Connection connection;
+
+}
